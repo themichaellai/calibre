58fd675cda943e479aea52f0ea9cfe34fbed1535
Kovid Goyal <kovid@kovidgoyal.net>
2018-08-07 20:20:25 +0530

    Add tests for the new viewer cache

--- setup/test.py
+++ setup/test.py
@@ -72,6 +72,8 @@ def find_tests(which_tests=None):
         a(find_tests())
         from calibre.utils.search_query_parser_test import find_tests
         a(find_tests())
+        from calibre.gui2.viewer2.convert_book import find_tests
+        a(find_tests())
     if ok('dbcli'):
         from calibre.db.cli.tests import find_tests
         a(find_tests())
